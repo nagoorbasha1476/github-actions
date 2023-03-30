e99adf0e101b2a4dbfefc24b3c60d69cc8b9f285
nagoorbasha1476 <128470718+nagoorbasha1476@users.noreply.github.com>
2023-03-30 22:36:14 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def main():
-    print("Hello, world!")
-    print("The value of the MY_SECRET environment variable is:", os.environ.get('MY_SECRET'))
-
-if __name__ == "__main__":
-    main()
